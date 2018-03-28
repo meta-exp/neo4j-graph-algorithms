940bbffad0a3926e47ebe6c3d084aefa1a23e6f3
Michaell Vaichenker <Michael.Vaichenker@student.hpi.de>
2018-03-28 11:55:41 +0200

    debugging: no compute

--- algo/src/main/java/org/neo4j/graphalgo/impl/computeAllMetaPaths/ComputeAllMetaPaths.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/computeAllMetaPaths/ComputeAllMetaPaths.java
@@ -117,9 +117,9 @@ public class ComputeAllMetaPaths extends Algorithm<ComputeAllMetaPaths> {
     private boolean initializeNode(int node, HashMap<Integer, Integer> labelCountDict) {
 
         int nodeLabel = arrayGraphInterface.getLabel(node);
-        labelCountDict.put(nodeLabel, 1 + (labelCountDict.get(nodeLabel) == null ? 0 : labelCountDict.get(nodeLabel)));//TODO: find error: index out of bound 14/13
+        labelCountDict.put(nodeLabel, 1 + (labelCountDict.get(nodeLabel) == null ? 0 : labelCountDict.get(nodeLabel)));
 
-        //initialInstances.get(nodeLabel).add(node);
+        initialInstances.get(nodeLabel).add(node);//TODO: find error: index out of bound 14/13
         return true;
     }
 
