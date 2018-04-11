cfb0aff38855503c0fa27da22fdbae8f63d72191
fswt <fabian.stolp@student.hpi.uni-potsdam.de>
2018-04-11 11:49:54 +0200

    add new thread class for from node computation

---
+++ algo/src/main/java/org/neo4j/graphalgo/impl/metaPathComputation/ComputeMetaPathFromNodeThread.java
@@ -0,0 +1,28 @@
+package org.neo4j.graphalgo.impl.metaPathComputation;
+
+public class ComputeMetaPathFromNodeThread extends Thread {
+    MetaPathComputation parent;
+    String threadName;
+    int nodeID;
+    int metaPathLength;
+
+    ComputeMetaPathFromNodeThread(MetaPathComputation parent, String threadName, int nodeID, int metaPathLength) {
+        this.parent = parent;
+        this.threadName = threadName;
+        this.nodeID = nodeID;
+        this.metaPathLength = metaPathLength;
+        currentThread().setName(threadName);
+    }
+
+    public void run() {
+        parent.computeMetaPathFromNodeLabel(nodeID, metaPathLength);
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    public int getNodeID() {
+        return nodeID;
+    }
+}
