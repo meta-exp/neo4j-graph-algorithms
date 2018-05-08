271029c26e053cc5987e4f09d655bb792a01a5c9
Michaell Vaichenker <Michael.Vaichenker@student.hpi.de>
2018-05-08 13:41:43 +0200

    Implemented Proc and Result

---
+++ algo/src/main/java/org/neo4j/graphalgo/metaPathComputationProcs/GetSchemaProc.java
@@ -0,0 +1,60 @@
+package org.neo4j.graphalgo.metaPathComputationProcs;
+
+import org.neo4j.graphalgo.core.GraphLoader;
+import org.neo4j.graphalgo.core.heavyweight.HeavyGraph;
+import org.neo4j.graphalgo.core.heavyweight.HeavyGraphFactory;
+import org.neo4j.graphalgo.impl.metaPathComputation.ComputeAllMetaPathsSchemaFull;
+import org.neo4j.graphalgo.impl.metaPathComputation.getSchema.GetSchema;
+import org.neo4j.graphalgo.impl.metaPathComputation.getSchema.Pair;
+import org.neo4j.graphalgo.results.metaPathComputationResults.ComputeAllMetaPathsSchemaFullResult;
+import org.neo4j.graphalgo.results.metaPathComputationResults.GetSchemaResult;
+import org.neo4j.kernel.api.KernelTransaction;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+import org.neo4j.logging.Log;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.stream.Stream;
+
+public class GetSchemaProc {
+    @Context
+    public GraphDatabaseAPI api;
+
+    @Context
+    public Log log;
+
+    @Context
+    public KernelTransaction transaction;
+
+    @Procedure("algo.GetSchema")
+    @Description("CALL algo.GetSchema() YIELD schema: \n" +
+            "return schema as adjacency list \n")
+
+    public Stream<GetSchemaResult> GetSchema() {
+
+        final GetSchemaResult.Builder builder = GetSchemaResult.builder();
+        final HeavyGraph graph;
+
+        graph = (HeavyGraph) new GraphLoader(api)
+                .asUndirected(true)
+                .withLabelAsProperty(true)
+                .load(HeavyGraphFactory.class);
+
+        final GetSchema algo = new GetSchema(graph);
+
+        GetSchema.Result result = algo.compute();
+        ArrayList<ArrayList<Pair>> schema = result.getSchemaAdjacencies();
+        HashMap<Integer, Integer> reverseDictionary = result.getReverseLabelDictionary();
+        builder.setSchema(schema);
+        builder.setReverseLabelDictionary(reverseDictionary);
+        graph.release();
+
+        return Stream.of(builder.build());
+
+    }
+}
---
+++ algo/src/main/java/org/neo4j/graphalgo/results/metaPathComputationResults/GetSchemaResult.java
@@ -0,0 +1,42 @@
+package org.neo4j.graphalgo.results.metaPathComputationResults;
+
+import com.google.gson.Gson;
+import org.neo4j.graphalgo.impl.metaPathComputation.getSchema.Pair;
+import org.neo4j.graphalgo.results.AbstractResultBuilder;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GetSchemaResult {
+
+    public final String schema;
+    public final String reverseLabelDictionary;
+
+    private GetSchemaResult(ArrayList<ArrayList<Pair>> schema, HashMap<Integer, Integer> reverseLabelDictionary) {
+        Gson gson = new Gson();
+        this.schema = gson.toJson(schema);
+        this.reverseLabelDictionary = gson.toJson(reverseLabelDictionary);
+    }
+
+    public static GetSchemaResult.Builder builder() {
+        return new GetSchemaResult.Builder();
+    }
+
+    public static class Builder extends AbstractResultBuilder<GetSchemaResult> {
+
+        private ArrayList<ArrayList<Pair>> schema;
+        private HashMap<Integer, Integer> reverseLabelDictionary;
+
+        public void setSchema(ArrayList<ArrayList<Pair>> schema) {
+            this.schema = schema;
+        }
+
+        public void setReverseLabelDictionary(HashMap<Integer, Integer> reverseLabelDictionary) {
+            this.reverseLabelDictionary = reverseLabelDictionary;
+        }
+
+        public GetSchemaResult build() {
+            return new GetSchemaResult(schema, reverseLabelDictionary);
+        }
+    }
+}
