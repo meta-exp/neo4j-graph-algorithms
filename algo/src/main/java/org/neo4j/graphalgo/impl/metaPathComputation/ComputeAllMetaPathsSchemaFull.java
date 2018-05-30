ebdf7f54f3f378de317270a16d9d084d627f7315
Michaell Vaichenker <Michael.Vaichenker@student.hpi.de>
2018-05-30 15:19:50 +0200

    Different debugmessages

--- algo/src/main/java/org/neo4j/graphalgo/impl/metaPathComputation/ComputeAllMetaPathsSchemaFull.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/metaPathComputation/ComputeAllMetaPathsSchemaFull.java
@@ -58,10 +58,8 @@ public class ComputeAllMetaPathsSchemaFull extends MetaPathComputation {
     public void computeMetaPathFromNodeLabel(int nodeID, int metaPathLength) {
         ArrayList<Integer> initialMetaPath = new ArrayList<>();
         initialMetaPath.add(reversedLabelDictionary.get(nodeID)); //because nodeID is already a type of nodes in the real graph//convert to heavyGraph nodeType
-        if(reversedLabelDictionary.get(nodeID) == null) System.out.println("ALARM");
-        System.out.println("we reached a thread.");
+        System.out.println(reversedLabelDictionary);
         addAndLogMetaPath(initialMetaPath);
-        System.out.println("we should have wrote something.");
         computeMetaPathFromNodeLabel(initialMetaPath, nodeID, metaPathLength - 1);
     }
 
