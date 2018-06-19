eff1909d2b297b91682f82d0a0066cf638c2c02f
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-06-19 19:30:15 +0200

    Add test for computation of meta-path

--- tests/src/test/java/org/neo4j/graphalgo/impl/metaPathComputationTests/ComputeAllMetaPathsBetweenInstancesTest.java
+++ tests/src/test/java/org/neo4j/graphalgo/impl/metaPathComputationTests/ComputeAllMetaPathsBetweenInstancesTest.java
@@ -1,7 +1,7 @@
 package org.neo4j.graphalgo.impl.metaPathComputationTests;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.neo4j.graphalgo.TestDatabaseCreator;
 import org.neo4j.graphalgo.core.GraphLoader;
@@ -15,11 +15,16 @@ import org.neo4j.kernel.api.exceptions.KernelException;
 import org.neo4j.kernel.impl.proc.Procedures;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Consumer;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ComputeAllMetaPathsBetweenInstancesTest {
 
@@ -27,8 +32,8 @@ public class ComputeAllMetaPathsBetweenInstancesTest {
     private ComputeAllMetaPathsBetweenInstances algo;
     private HeavyGraph graph;
 
-    @BeforeClass
-    public static void setup() throws KernelException{
+    @Before
+    public void setup() throws KernelException{
 
         api = TestDatabaseCreator.createTestDatabase();
 
@@ -44,8 +49,8 @@ public class ComputeAllMetaPathsBetweenInstancesTest {
         }
     }
 
-    @AfterClass
-    public static void shutdownGraph() throws Exception {
+    @After
+    public void shutdownGraph() throws Exception {
         api.shutdown();
     }
 
@@ -71,6 +76,40 @@ public class ComputeAllMetaPathsBetweenInstancesTest {
         runQuery("MATCH (n1 {name: 'a'}), (n2 {name: 'c'}) RETURN ID(n1) as id_n1, ID(n2) as id_n2",
                 row -> assertFalse((new File("between_instances/MetaPaths_" + row.getNumber("id_n1").longValue() + "_" + row.getNumber("id_n2").longValue() + ".txt").exists())));
 
+    }
+
+    @Test
+    public void testComputationMetaPath() {
+        String cypher = "CREATE (a:A {name:\"a\"})\n" +
+                "CREATE (b:B {name:\"b\"})\n" +
+                "CREATE\n" +
+                "  (a)-[:TYPE1]->(b)";
+        run_query(cypher);
+
+        graph = (HeavyGraph) new GraphLoader(api)
+                .asUndirected(true)
+                .withLabelAsProperty(true)
+                .load(HeavyGraphFactory.class);
+
+        algo = new ComputeAllMetaPathsBetweenInstances(graph, 3);
+        algo.compute();
+
+        String metapath = "0|0|0";
+
+        runQuery("MATCH (n1 {name: 'a'}), (n2 {name: 'b'}) RETURN ID(n1) as id_n1, ID(n2) as id_n2", row -> {
+            Path file = FileSystems.getDefault().getPath("between_instances", "MetaPaths_" + row.getNumber("id_n1").longValue() + "_" + row.getNumber("id_n2").longValue() + ".txt");
+            try {
+                try (BufferedReader reader = Files.newBufferedReader(file, Charset.forName("US-ASCII"))) {
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+                        assertEquals(metapath, line);
+					}
+				}
+            } catch (IOException e) {
+                e.printStackTrace();
+                fail("Exception thrown");
+            }
+        });
     }
 
     private void runQuery(
